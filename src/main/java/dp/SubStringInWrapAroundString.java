1d0a051fc74200e306c859ce648886611b545e0a
indiasaurabh@hotmail.com <indiasaurabh@hotmail.com>
2018-01-30 00:51:49 +0530

    add

--- src/main/java/dp/SubStringInWrapAroundString.java
+++ src/main/java/dp/SubStringInWrapAroundString.java
@@ -7,46 +7,55 @@ import java.util.ArrayList;
  */
 public class SubStringInWrapAroundString {
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
 
 
-        String input="zab";
-        System.out.print(allPossibleCobmbinations(input));
-
-
-
-
+        String input = "cac";
+        ArrayList<String> values=new ArrayList<String>();
+        System.out.print(allPossibleCobmbinations(input, values));
     }
 
-
-    public static int findSubstringInWraproundString(String p) {
+    public static int allPossibleCobmbinations(String input, ArrayList<String> values) {
+        char[] ch = input.toCharArray();
+        int count = 0;
+        for (int i = 0; i < ch.length; i++) {
+            count += allcombo("", i, ch, values);
+        }
+        count = count;
+        System.out.println("count " + count);
 
         return 0;
+
     }
 
-    public static ArrayList<String> allPossibleCobmbinations(String input){
-        char [] ch=input.toCharArray();
-        ArrayList<String> allPossible=new ArrayList<String>();
-        int i=0;
-        for(;i<ch.length-2;i++){
-            allPossible.add(ch[i]+"");
-            allcombination(allPossible, ch,i++,ch[i]+"");
+    public static int allcombo(String in, int j, char[] sh, ArrayList<String> values) {
+        int count = 0;
+        if (j < sh.length) {
+
+
+            String s = in + sh[j];
+            if (isSub(s)) {
+                if(!values.contains(s)) {
+                    values.add(s);
+                    count = count + allcombo(s, j + 1, sh, values) + 1;
+                }
+                else
+                    count = count + allcombo(s, j + 1, sh, values) + 0;
+            }
+
+
         }
-        allPossible.add(ch[i]+"");
-
-
-
-
-        return allPossible;
+        return count;
 
     }
 
-    public static void allcombination(ArrayList<String> allcom, char [] ch, int start, String intial){
-        if(start<ch.length-1)
-        allcombination(allcom, ch,start++, intial+ch[start]);
 
+    public static boolean isSub(String in) {
+
+        String s="zabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcd";
+
+        return s.contains(in);
     }
-
 
 
 }
---
+++ src/main/java/dp/UniqueBST.java
@@ -0,0 +1,20 @@
+package dp;
+
+/**
+ * Created by saurabh on 30/1/18.
+ */
+public class UniqueBST {
+
+
+    public static void main(String [] args){
+
+
+
+
+    }
+
+
+
+
+
+}
